a3c4cca251a8770aebd6593b9d18d33b32b8d00c
suk063 <suk063@ucsd.edu>
2025-08-14 12:33:37 -0700

    minor changes

--- lang_mapping/agent/agent_uplifted_bc.py
+++ lang_mapping/agent/agent_uplifted_bc.py
@@ -129,7 +129,7 @@ class Agent_uplifted_bc(nn.Module):
         visual_tok = self.transformer_encoder(
             visual_token=feats,
             coords=coords,  
-            use_pe=True
+            use_pe=False
         ) 
         
         action_out = self.action_transformer(visual_tok, state_tok, text_emb)
